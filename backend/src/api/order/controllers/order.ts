3aaa1495d4f952ebc935759b1b53c3235a3bf438
imniloy <imniloy8@gmail.com>
2023-09-24 22:00:39 +0600

    checkout page on progress

--- backend/src/api/order/controllers/order.ts
+++ backend/src/api/order/controllers/order.ts
@@ -1,59 +1,7 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-import { factories } from "@strapi/strapi";
+/**
+ * order controller
+ */
 
-export default factories.createCoreController(
-  "api::order.order",
-  ({ strapi }) => ({
-    async create(ctx) {
-      const { products, userId } = ctx.request.body;
+import { factories } from '@strapi/strapi'
 
-      try {
-        const lineItems = await Promise.all(
-          products.map(async (product) => {
-            const item = await strapi
-              .service("api::product.product")
-              .findOne(product?.id);
-
-            return {
-              price_data: {
-                currency: "usd",
-                product_data: {
-                  name: item.name,
-                },
-                unit_amount: Math.round(item.price * 100),
-              },
-              quantity: product.quantity,
-            };
-          })
-        );
-
-        const session = await stripe.checkout.sessions.create({
-          payment_method_types: ["card"],
-          line_items: lineItems,
-          mode: "payment",
-          success_url: process.env.CLIENT_URL + `/order/congratulation`,
-          cancel_url: process.env.CLIENT_URL,
-        });
-
-        await strapi.service("api::order.order").create({
-          data: {
-            products,
-            stripeId: session.id,
-            status: "pending",
-            users_permissions_user: userId,
-          },
-        });
-
-        return { stripeId: session.id };
-      } catch (error) {
-        if (!products) {
-          ctx.response.status = 400;
-          return new Error("Products not found");
-        } else {
-          ctx.response.status = 500;
-          return error;
-        }
-      }
-    },
-  })
-);
+export default factories.createCoreController('api::order.order');
